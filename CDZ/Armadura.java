c1fffef3e9dbb26b1505c7df1cfdee69a0c9a870
Lucas Damaceno <lucas.damaceno@cwi.com.br>
2017-04-13 17:14:17 -0300

    feat(CDZ):Adicionados Enums

--- CDZ/Armadura.java
+++ CDZ/Armadura.java
@@ -1,6 +1,8 @@
 public class Armadura {
 private String constelacao;
+private Categoria categoria;
 public Armadura(String constelacao){
 this.constelacao = constelacao;
+this.categoria = Categoria.BRONZE;
 }
 }
