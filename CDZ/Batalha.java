3ea6235e20a924f62fdd35c831c7da412a367b30
Lucas Damaceno <lucas.damaceno@cwi.com.br>
2017-04-14 15:00:50 -0300

    fix(CDZ-Modulo1-Batalha):Mudada visibilidade das vars membras da class

--- CDZ/Batalha.java
+++ CDZ/Batalha.java
@@ -1,6 +1,6 @@
 public class Batalha{
-public Saint saint1;
-public Saint saint2;
+private Saint saint1;
+private Saint saint2;
 public Batalha(Saint saint1, Saint saint2){
     this.saint1 = saint1;
     this.saint2 = saint2;
