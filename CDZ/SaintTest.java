5a061f0f51865010d562eb3840a6189813cecb4f
Lucas Damaceno <lucas.damaceno@cwi.com.br>
2017-04-15 15:05:37 -0300

    test(CategorizaçãoArmaduras):Adicionar novos testes de armaduras.

--- CDZ/SaintTest.java
+++ CDZ/SaintTest.java
@@ -9,86 +9,96 @@ public class SaintTest{
     /* AAA 
     1. Arrange - Montagem dos dados de teste.
     */
-        Armadura estalactite = new Armadura ("Estalactite",Categoria.OURO);
-        Saint shun = new Saint("Shun",estalactite);
+        Armadura pegaso = new Armadura ("Pegaso",Categoria.BRONZE);
+        Saint seiya = new Saint("Seiya", pegaso);
     /*
     2. Act - Invocar a ação a ser testada.
     */
-        shun.vestirArmadura();
-        boolean resultado = shun.getArmaduraVestida();
+        seiya.vestirArmadura();
+        boolean resultado = seiya.getArmaduraVestida();
    /*
     3. Assert - Verificação dos resultados do teste.
     */ 
         assertEquals(true, resultado);
     }
-	@Test 
-	public void garantirQueMetodoDespirArmaduraFuncione(){
-		Saint shun = new Saint("Shun", new Armadura("Andrômeda",Categoria.BRONZE));
-		shun.vestirArmadura();
-		shun.despirArmadura();
-		assertEquals(false, shun.getArmaduraVestida());
-	}
+    @Test 
+    public void garantirQueMetodoDespirArmaduraFuncione(){
+        Saint shun = new Saint("Shun", new Armadura("Andrômeda",Categoria.BRONZE));
+        shun.vestirArmadura();
+        shun.despirArmadura();
+        assertEquals(false, shun.getArmaduraVestida());
+    }
     @Test
     public void naoVestirArmaduraDeixaArmaduraNaoVestida(){
-    	Saint hyoga = new Saint("Hyoga", new Armadura("Cisne",Categoria.BRONZE));
-    	assertEquals(false, hyoga.getArmaduraVestida());
+        Saint hyoga = new Saint("Hyoga", new Armadura("Cisne",Categoria.BRONZE));
+        assertEquals(false, hyoga.getArmaduraVestida());
     }
     @Test
     public void garantirQueSaintNascaVivo(){
-    	Saint shun = new Saint("Shun", new Armadura ("Andrômeda",Categoria.BRONZE));
-    	assertEquals(Status.VIVO, shun.getStatus());
+        Saint shun = new Saint("Shun", new Armadura ("Andrômeda",Categoria.BRONZE));
+        assertEquals(Status.VIVO, shun.getStatus());
     }
     @Test
-    public void garantirQueGeneroDefaultSejaNaoIdentificado(){
-    	Saint ikki = new Saint("Ikki", new Armadura ("Fênix",Categoria.BRONZE));
-    	assertEquals(Genero.NAO_INFORMADO, ikki.getGenero());
+    public void garantirQueGeneroDefaultSejaNaoInformado(){
+        Saint ikki = new Saint("Ikki", new Armadura ("Fênix",Categoria.BRONZE));
+        assertEquals(Genero.NAO_INFORMADO, ikki.getGenero());
     }
     @Test
     public void garantirQueTrocarGeneroTroqueDeFatoOGenero(){
-    	Saint shun = new Saint("Shun", new Armadura ("Andrômeda", Categoria.BRONZE));
-    	shun.setGenero(Genero.FEMININO);
-    	assertEquals(Genero.FEMININO, shun.getGenero());
+        Saint shun = new Saint("Shun", new Armadura ("Andrômeda", Categoria.BRONZE));
+        shun.setGenero(Genero.FEMININO);
+        assertEquals(Genero.FEMININO, shun.getGenero());
     }
     @Test
     public void garantirQueTrocarStatusTroqueDeFatoOStatus(){
-    	Saint jabu = new Saint("Jabu", new Armadura ("Unicórnio", Categoria.BRONZE));
-    	jabu.setStatus(Status.DESACORDADO);
-    	assertEquals(Status.DESACORDADO, jabu.getStatus());
+        Saint jabu = new Saint("Jabu", new Armadura ("Unicórnio", Categoria.BRONZE));
+        jabu.setStatus(Status.DESACORDADO);
+        assertEquals(Status.DESACORDADO, jabu.getStatus());
     }
-	@Test
-	public void garantirQueStatusNaoSejaEstatico(){
-		Saint jabu = new Saint("Jabu", new Armadura ("Unicórnio", Categoria.BRONZE));
-    	jabu.setStatus(Status.MORTO);
-    	assertEquals(Status.MORTO, jabu.getStatus());
-	}
-	
+    @Test
+    public void garantirQueStatusNaoSejaEstatico(){
+        Saint jabu = new Saint("Jabu", new Armadura ("Unicórnio", Categoria.BRONZE));
+        jabu.setStatus(Status.MORTO);
+        assertEquals(Status.MORTO, jabu.getStatus());
+    }
+    
     @Test
     public void garantirQueFuncaoPerderVidaEstejaCalculandoCorretamente(){
-    	Saint ichi = new Saint("Ichi", new Armadura("Hidra", Categoria.BRONZE));
-    	ichi.perderVida(23.2);
-    	//assertEquals(expected, actual, delta) utilizado abaixo, para comparar valor obtido com o esperado.
-    	// parâmetro delta é a tolerância aceita.
-    	assertEquals(ichi.getVida(),76.8,0.01);
+        Saint ichi = new Saint("Ichi", new Armadura("Hidra", Categoria.BRONZE));
+        ichi.perderVida(23.2);
+        //assertEquals(expected, actual, delta) utilizado abaixo, para comparar valor obtido com o esperado.
+        // parâmetro delta é a tolerância aceita.
+        assertEquals(ichi.getVida(),76.8,0.01);
     }
     @Test
     public void garantirQueVidaPadraoSeja100(){
-    	Saint kanon = new Saint("Kanon", new Armadura("Gêmeos", Categoria.OURO));
-    	assertEquals(kanon.getVida(),100.0,0.01);
+        Saint kanon = new Saint("Kanon", new Armadura("Gêmeos", Categoria.OURO));
+        assertEquals(kanon.getVida(),100.0,0.01);
     }
     @Test
-    public void garantirQuePesoDaArmaduraSejaAdquirido(){
-    	Saint mu = new Saint("Mu", new Armadura("Áries", Categoria.PRATA));
-    	assertEquals(mu.getCategoriaArmadura(), 2);
+    public void garantirQuePesoDaArmaduraSejaAdquiridoCorretamenteParaCavaleiroDeOuro(){
+        Saint aiolia = new Saint("Aiolia", new Armadura("Leão", Categoria.OURO));
+        assertEquals(aiolia.getCategoriaArmadura(), 3);
     }
-    
-	@Test
-	public void garantirQueMetodoGetConstelacaoDoSaintFuncioneAdequadamente(){
-		Saint ikki = new Saint("Ikki", new Armadura("Fênix",Categoria.PRATA));
-		assertEquals(ikki.getConstelacao(),"Fênix");
-	}
-	@Test
-	public void garantirQueMetodoGetConstelacaoDaArmaduraFuncioneAdequadamente(){
-		Armadura aries = new Armadura("Aries", Categoria.OURO);
-		assertEquals(aries.getConstelacao(),"Aries");
-	}
+    @Test
+    public void garantirQuePesoDaArmaduraSejaAdquiridoCorretamenteParaCavaleiroDePrata(){
+        Saint misty = new Saint("Misty", new Armadura("Lagarto", Categoria.PRATA));
+        assertEquals(misty.getCategoriaArmadura(), 2);
+    }
+    @Test
+    public void garantirQuePesoDaArmaduraSejaAdquiridoCorretamenteParaCavaleiroDeBronze(){
+        Saint shiryu = new Saint("Shiryu", new Armadura("Dragão", Categoria.BRONZE));
+        assertEquals(shiryu.getCategoriaArmadura(), 1);
+    }
+
+    @Test
+    public void garantirQueMetodoGetConstelacaoDoSaintFuncioneAdequadamente(){
+        Saint ikki = new Saint("Ikki", new Armadura("Fênix",Categoria.BRONZE));
+        assertEquals(ikki.getConstelacao(),"Fênix");
+    }
+    @Test
+    public void garantirQueMetodoGetConstelacaoDaArmaduraFuncioneAdequadamente(){
+        Armadura aries = new Armadura("Aries", Categoria.OURO);
+        assertEquals(aries.getConstelacao(),"Aries");
+    }
 }
