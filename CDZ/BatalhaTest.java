f5fc1a5c9eb50443938fda6f6c78de8d21645c69
Lucas Damaceno <lucas.damaceno@cwi.com.br>
2017-04-14 23:59:13 -0300

    test(Batalha):Implementar classe de testes de Batalha
    Testes de batalha retirados da classe SaintTest e inseridos na nova classe BatalhaTest.

---
+++ CDZ/BatalhaTest.java
@@ -0,0 +1,27 @@
+
+
+import static org.junit.Assert.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class BatalhaTest{
+
+@Test
+    public void garantirQueCavaleiroDeArmaduraMelhorAtaquePrimeiro(){
+    	Saint ikki = new Saint("Ikki", new Armadura("Fênix",Categoria.PRATA));
+    	Saint kanon = new Saint("Kanon", new Armadura("Gêmeos", Categoria.OURO));
+    	Batalha batalha1 = new Batalha(ikki,kanon);
+    	batalha1.iniciar();
+    	assertEquals(ikki.getVida(),90.0, 0.00001);
+    }
+    @Test
+    public void garantirQueCasoArmadurasSejamIguaisOPrimeiroAtaque(){
+    	Saint ikki = new Saint("Ikki", new Armadura("Fênix",Categoria.PRATA));
+    	Saint kanon = new Saint("Kanon", new Armadura("Gêmeos", Categoria.PRATA));
+    	Batalha batalha1 = new Batalha(ikki,kanon);
+    	batalha1.iniciar();
+    	assertEquals(kanon.getVida(),90.0, 0.00001);
+    }
+
+}
