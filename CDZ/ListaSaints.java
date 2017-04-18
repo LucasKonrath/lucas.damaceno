9932fb7a03a238d64e3cfcd8b3f3ccab1c5743c5
Lucas Damaceno <lucas.damaceno@cwi.com.br>
2017-04-18 17:40:33 -0300

    Feat(ListaSaints):Adicionado rascunho da classe ListaSaints.

---
+++ CDZ/ListaSaints.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+
+public class ListaSaints{
+    private ArrayList<Saint> listaDeSaints;
+    public ArrayList<Saint> adicionaSaint(Saint saint){
+    listaDeSaints.add(saint);
+    return this.listaDeSaints;
+    }
+}
