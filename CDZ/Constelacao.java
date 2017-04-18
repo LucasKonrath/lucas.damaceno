c0f3f79b2e459d507e867aabc399d45b6f5a9ec6
Lucas Damaceno <lucas.damaceno@cwi.com.br>
2017-04-18 00:54:57 -0300

    refactor(Constelacao):Melhorada identação e logica condicional

--- CDZ/Constelacao.java
+++ CDZ/Constelacao.java
@@ -11,7 +11,7 @@ public class Constelacao{
     public void aprenderGolpe(Golpe golpe){
         meusGolpes[indiceAPopular] = golpe;
         if(indiceAPopular<meusGolpes.length-1){
-        this.indiceAPopular++;
+            this.indiceAPopular++;
         }
         else ultimoCadastrado=true;
     }
@@ -19,22 +19,25 @@ public class Constelacao{
         return this.nome;
     }
     public Golpe[] getGolpes(){
-    return this.meusGolpes;
+        return this.meusGolpes;
     }
     public Golpe getProximoGolpe() throws Exception{
     
-    if (indiceADevolverGolpe==0 && indiceAPopular==0) throw new Exception ("Nenhum golpe cadastrado");
-    else if (indiceADevolverGolpe==0 && indiceAPopular==1) indiceADevolverGolpe=0;
-    else if (indiceADevolverGolpe==0 && indiceAPopular==2){ 
-        if (ultimoGolpeFoiZero==true) indiceADevolverGolpe=1;
+        if (indiceADevolverGolpe==0){
+            if (indiceAPopular==0) throw new Exception ("Nenhum golpe cadastrado");
+            else if (indiceAPopular==1) indiceADevolverGolpe=0;
+            else if (indiceAPopular==2){ 
+                if (ultimoGolpeFoiZero==true) indiceADevolverGolpe=1;
+                else indiceADevolverGolpe=0;
+            }
+        }
+        else if (indiceADevolverGolpe==1 && indiceAPopular==2 && ultimoCadastrado==true) indiceADevolverGolpe=2;
         else indiceADevolverGolpe=0;
-    }
-    else if (indiceADevolverGolpe==1 && indiceAPopular==2 && ultimoCadastrado==true) indiceADevolverGolpe=2;
-    else indiceADevolverGolpe=0;
-    if (indiceADevolverGolpe==0) ultimoGolpeFoiZero = true;
-    else ultimoGolpeFoiZero = false;
-    Golpe golpeADevolver = this.meusGolpes[indiceADevolverGolpe];
-    return golpeADevolver;
+    
+        if (indiceADevolverGolpe==0) ultimoGolpeFoiZero = true;
+        else ultimoGolpeFoiZero = false;
+        Golpe golpeADevolver = this.meusGolpes[indiceADevolverGolpe];
+        return golpeADevolver;
     }
 
 }
