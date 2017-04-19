5e80a07f26800a3b9f1cc3bafe86675a08b23fb4
Lucas Damaceno <lucas.damaceno@cwi.com.br>
2017-04-19 15:27:45 -0300

    test(ListaSaints):Adicionados teste ao metodo que categoriza Saints.

--- CDZ/ListaSaintsTest.java
+++ CDZ/ListaSaintsTest.java
@@ -46,5 +46,86 @@ public void garantirQueMetodoTodosFuncioneNormalmente()throws Exception{
     ArrayList<Saint> teste = lista.todos();
     assertEquals(lista.todos(), teste);
 }
-
+@Test
+public void buscarPorNomeFuncionaAdequadamente() throws Exception{
+    ListaSaints lista = new ListaSaints();
+    Constelacao cisne = new Constelacao("Cisne");
+    Armadura deCisne = new Armadura(cisne,Categoria.BRONZE);
+    Constelacao fenix = new Constelacao("Fenix");
+    Armadura deFenix = new Armadura(cisne,Categoria.BRONZE);
+    Constelacao lagarto = new Constelacao("Lagarto");
+    Armadura deLagarto = new Armadura(lagarto, Categoria.PRATA);
+    BronzeSaint ikki = new BronzeSaint("Ikki", deFenix);
+    BronzeSaint hyoga = new BronzeSaint("Hyoga", deCisne);
+    BronzeSaint misty = new BronzeSaint("Misty", deLagarto);
+    lista.adicionaSaint(ikki);
+    lista.adicionaSaint(hyoga);
+    lista.adicionaSaint(misty);
+    Saint definitivamenteNaoEOHyoga = lista.buscarPorNome("Hyoga");
+    assertEquals(hyoga,definitivamenteNaoEOHyoga);
 }
+@Test public void buscarSaintExistenteComRepeticao()throws Exception{
+    ListaSaints lista = new ListaSaints();
+    Constelacao cisne = new Constelacao("Cisne");
+    Armadura deCisne = new Armadura(cisne,Categoria.BRONZE);
+    Constelacao fenix = new Constelacao("Fenix");
+    Armadura deFenix = new Armadura(cisne,Categoria.BRONZE);
+    Constelacao lagarto = new Constelacao("Lagarto");
+    Armadura deLagarto = new Armadura(lagarto, Categoria.PRATA);
+    BronzeSaint ikki = new BronzeSaint("Ikki", deFenix);
+    BronzeSaint hyoga = new BronzeSaint("Hyoga", deCisne);
+    BronzeSaint misty = new BronzeSaint("Hyoga", deLagarto);
+    lista.adicionaSaint(ikki);
+    lista.adicionaSaint(hyoga);
+    lista.adicionaSaint(misty);
+    Saint definitivamenteNaoEOHyoga = lista.buscarPorNome("Hyoga");
+    assertEquals(hyoga,definitivamenteNaoEOHyoga);
+}
+@Test
+public void buscarPorNomeRetornaNullSeNaoHouverSaint() throws Exception{
+    ListaSaints lista = new ListaSaints();
+    Constelacao cisne = new Constelacao("Cisne");
+    Armadura deCisne = new Armadura(cisne,Categoria.BRONZE);
+    Constelacao fenix = new Constelacao("Fenix");
+    Armadura deFenix = new Armadura(cisne,Categoria.BRONZE);
+    Constelacao lagarto = new Constelacao("Lagarto");
+    Armadura deLagarto = new Armadura(lagarto, Categoria.PRATA);
+    BronzeSaint ikki = new BronzeSaint("Ikki", deFenix);
+    BronzeSaint hyoga = new BronzeSaint("Hyoga", deCisne);
+    BronzeSaint misty = new BronzeSaint("Misty", deLagarto);
+    lista.adicionaSaint(ikki);
+    lista.adicionaSaint(hyoga);
+    lista.adicionaSaint(misty);
+    Saint definitivamenteNaoEOHyoga = lista.buscarPorNome("Yoda");
+    assertEquals(null,definitivamenteNaoEOHyoga);
+}
+@Test 
+public void buscarPorNomeEmListaVaziaRetornaNull(){
+assertNull(new ListaSaints().buscarPorNome("Seiya"));
+}
+@Test
+public void garantirFuncionamentoDaBuscaPorCategoria()throws Exception{
+    ListaSaints lista = new ListaSaints();
+    Constelacao cisne = new Constelacao("Cisne");
+    Armadura deCisne = new Armadura(cisne,Categoria.BRONZE);
+    Constelacao fenix = new Constelacao("Fenix");
+    Armadura deFenix = new Armadura(cisne,Categoria.BRONZE);
+    Constelacao lagarto = new Constelacao("Lagarto");
+    Armadura deLagarto = new Armadura(lagarto, Categoria.PRATA);
+    BronzeSaint ikki = new BronzeSaint("Ikki", deFenix);
+    BronzeSaint hyoga = new BronzeSaint("Hyoga", deCisne);
+    BronzeSaint misty = new BronzeSaint("Misty", deLagarto);
+    lista.adicionaSaint(ikki);
+    lista.adicionaSaint(hyoga);
+    lista.adicionaSaint(misty);
+    ArrayList<Saint> bronze = lista.buscarPorCategoria(Categoria.BRONZE);
+    lista.remover(misty);
+    ArrayList<Saint> todos = lista.todos();
+    assertEquals(bronze, todos);
+}
+@Test public void buscaPorCategoriaComListaVaziaRetornaNull(){
+    ListaSaints lista = new ListaSaints();
+    assertEquals(lista.buscarPorCategoria(Categoria.BRONZE), new ArrayList<Saint>());
+}
+}
+
