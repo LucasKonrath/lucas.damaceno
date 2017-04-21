23845bf58d3b08970f9e1d91d6aa20b7e8c4362d
Lucas Damaceno <lucas.damaceno@cwi.com.br>
2017-04-21 17:34:43 -0300

    feat(Batalha):Implementada lógica base das batalhas por turnos.
    Ainda falta: refatorar melhor o código, está muito bruto.
    Possivelmente evitar a repetição do movimento de vestirArmadura(talvez deletá-lo do array de movimentos depois que for executado uma vez?)

--- CDZ/Batalha.java
+++ CDZ/Batalha.java
@@ -1,26 +1,37 @@
 public class Batalha{
 
-  private Saint saint1, saint2;
-// Construtor da classe Batalha.
-  public Batalha(Saint saint1, Saint saint2){
-    this.saint1 = saint1;
-    this.saint2 = saint2;
-  }
-
-  public  void iniciar() throws Exception{ 
-	final double dano = 10.0;
-// Se categoria da armadura do saint 1 for maior ou igual a do Saint 2, ele ataca primeiro.
-    if(saint1.getCategoriaArmadura() >= saint2.getCategoriaArmadura()){
-      saint2.perderVida(dano);
-      System.out.println(saint1 + " ataca " +
-      saint2 + " causando " + dano + " de dano.");
+    private Saint saint1, saint2;
+    // Construtor da classe Batalha.
+    public Batalha(Saint saint1, Saint saint2){
+        this.saint1 = saint1;
+        this.saint2 = saint2;
     }
-// Senão, saint 2 ataca-o.
-    else{
-      saint1.perderVida(dano);
-      System.out.println(saint2 + " ataca " +
-      saint1 + " causando " + dano + " de dano.");
-    }
-  }
+    
+    
+    // LEMBRETE À MINHA PŔOPRIA PESSOA: REFATORAR ESTE MÉTODO, PELO AMOR DE DEUS. APENAS VEJA SE A LÓGICA FUNCIONA E DEPOIS DESTRUA ESTE MONSTRO.
+    public  void iniciar() throws Exception{ 
+        final double dano = 10.0;
+        Movimento movimento;
+        while(saint1.getStatus() != Status.MORTO && saint2.getStatus() != Status.MORTO){
+            // Se categoria da armadura do saint 1 for maior ou igual a do Saint 2, ele ataca primeiro.
+            if(saint1.getCategoriaArmadura() >= saint2.getCategoriaArmadura()){ 
+                movimento = saint1.getProximoMovimento();
+                movimento.executar();
+                if(saint2.getStatus() != Status.MORTO){
+                    movimento = saint2.getProximoMovimento();
+                    movimento.executar();
+                }
+            }
+            // Senão, saint 2 ataca-o.
+            else{
+                movimento = saint2.getProximoMovimento();
+                movimento.executar();
+                if(saint1.getStatus() != Status.MORTO){
+                   movimento = saint1.getProximoMovimento();
+                   movimento.executar();
+                }
+            }
+        }
 
+    }
 }
