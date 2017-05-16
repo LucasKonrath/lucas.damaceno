8c05407e745c5641c96343078c05b00b8e847e1c
Lucas Damaceno <lucas.damaceno@cwi.com.br>
2017-05-16 13:55:13 -0300

    fix(Modificar função para aceitar 0 como parâmetro).

--- Modulo4_JavaScript/js/Ex1605.js
+++ Modulo4_JavaScript/js/Ex1605.js
@@ -1,4 +1,6 @@
 function converterDecimal(numero, numeroCasas){
-numeroCasas = numeroCasas || 2;
+if(numeroCasas !== 0){
+numeroCasas =  numeroCasas || 2;
+}
 return parseFloat(numero.toFixed(numeroCasas));
 }
