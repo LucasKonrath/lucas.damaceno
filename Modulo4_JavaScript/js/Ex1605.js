e1d310fcfd8afc078d24dc7550df003fab1309fc
Lucas Damaceno <lucas.damaceno@cwi.com.br>
2017-05-16 14:32:39 -0300

    feat(Ex1605):adicionar funcao arredondar usando Math functions no number

--- Modulo4_JavaScript/js/Ex1605.js
+++ Modulo4_JavaScript/js/Ex1605.js
@@ -4,3 +4,10 @@ numeroCasas =  numeroCasas || 2;
 }
 return parseFloat(numero.toFixed(numeroCasas));
 }
+
+Number.prototype.arredondar = function converterComPow(numeroCasas){
+if(numeroCasas !== 0){
+numeroCasas =  numeroCasas || 2;
+}
+ return Math.round(this * Math.pow(10,numeroCasas))/Math.pow(10,numeroCasas);
+}
