3386919f6af75594b43ade5e10433fc73d5387f1
Lucas Damaceno <lucas.damaceno@cwi.com.br>
2017-05-17 00:54:26 -0300

    feat(ExPokemon):adicionar inicio do exercicio de PokeSearch.

---
+++ Modulo4_JavaScript/Pokemon/js/pokemon.js
@@ -0,0 +1,28 @@
+var value = 0;
+var musica = document.getElementById("audioPokemon"); 
+var nomePokemon = document.getElementById('nomePokemon');
+var numeroPokemon = document.getElementById('numeroPokemon');
+var imgPokemon = document.getElementById('img-pokemon');
+var input = document.getElementById('inputIDPokemon');
+var botao = document.getElementById('botaoEnvio');
+var divPokemon = document.getElementById('divPokemon')
+botao.onclick = function(){fetch('https://pokeapi.co/api/v2/pokemon/'+input.value+'/').then(function(response) { 
+    // Convert to JSON
+    return response.json();
+}).then(function(j) {
+    // Yay, `j` is a JavaScript object
+    objJSON = j; 
+    imgPokemon.src = j.sprites.front_default;
+    imgPokemon.style.filter="brightness(0%)";
+    nomePokemon.innerText = "Nome: " +  j.name;
+    numeroPokemon.innerText = "Numero: " + j.id;
+    divPokemon.style.visibility="visible";
+    musica.play();
+    setInterval(function changebrightness() {
+    console.log(this.value);
+    this.value += 2;
+   if(this.value <=100) imgPokemon.setAttribute("style","-webkit-filter:brightness("+value+"%)");
+    else clearInterval(1);
+},250);
+});};
+
