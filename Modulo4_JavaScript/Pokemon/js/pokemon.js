bfda16339b20a064787009df902f4bae6b03b1e2
Lucas Damaceno <lucas.damaceno@cwi.com.br>
2017-05-17 01:05:51 -0300

    fix(pokeSearch):melhorar logica utilizada na variação de brightness.

--- Modulo4_JavaScript/Pokemon/js/pokemon.js
+++ Modulo4_JavaScript/Pokemon/js/pokemon.js
@@ -12,17 +12,18 @@ botao.onclick = function(){fetch('https://pokeapi.co/api/v2/pokemon/'+input.valu
 }).then(function(j) {
     // Yay, `j` is a JavaScript object
     objJSON = j; 
+    this.value=0;
     imgPokemon.src = j.sprites.front_default;
     imgPokemon.style.filter="brightness(0%)";
     nomePokemon.innerText = "Nome: " +  j.name;
     numeroPokemon.innerText = "Numero: " + j.id;
     divPokemon.style.visibility="visible";
     musica.play();
-    setInterval(function changebrightness() {
+    var thisInterval = setInterval(function changebrightness() {
     console.log(this.value);
     this.value += 2;
    if(this.value <=100) imgPokemon.setAttribute("style","-webkit-filter:brightness("+value+"%)");
-    else clearInterval(1);
-},250);
+    else clearInterval(thisInterval);
+},500);
 });};
 
