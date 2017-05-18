08281a478698a8ffaa09758f74fa428e1020ea5a
Lucas Damaceno <lucas.damaceno@cwi.com.br>
2017-05-18 15:24:44 -0300

    feat(Exercicio2):adicionar exercicio de formatação de datas.

---
+++ Modulo5_AngularJS/Exercicio 2/controller.js
@@ -0,0 +1,18 @@
+var modulo = angular.module('Exercicio2',[]);
+
+modulo.controller('Ex2', function($scope,$filter){
+    
+    $scope.data='18/05/2017';
+    $scope.converter = converter;
+    $scope.dataConvertida;
+function converter(){
+var stringData=$scope.data;
+var splitData = stringData.split('/');
+var dataConvertida = new Date(splitData[2], splitData[1]-1, splitData[0]);
+dataConvertida = dataConvertida.toLocaleDateString("en-US");
+$scope.dataConvertida = $filter('date')(dataConvertida,'shortDate');
+};
+
+
+});
+
