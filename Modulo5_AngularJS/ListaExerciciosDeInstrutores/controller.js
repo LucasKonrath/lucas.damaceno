e9086972d4c61bdce546c7e396b33ee7774d884b
Lucas Damaceno <lucas.damaceno@cwi.com.br>
2017-05-19 01:18:38 -0300

    feat(ListaExercicios_AngularJS):adicionar lista de exercicios de instrutores.

---
+++ Modulo5_AngularJS/ListaExerciciosDeInstrutores/controller.js
@@ -0,0 +1,74 @@
+var modulo = angular.module('ListaExercicios',[]);
+
+
+
+modulo.filter('mascada',function(){
+
+              return function(nome){
+                
+            return nome.replace(/([n])([u])([n])([e])([s])/gi,'$' + nome + '$');
+        }});
+
+modulo.filter('filtrarAula',function(){
+
+              return function(aula){
+                
+            return padNumDigitos(aula.numero,3) + "   -   " + aula.nome;
+        }});
+
+function padNumDigitos(numero, digitos) {
+    return Array(Math.max(digitos - String(numero).length + 1, 0)).join(0) + numero;
+}
+
+
+modulo.controller('Lista', function($scope,$filter){
+    
+    
+    let instrutores = [{
+    nome: 'Bernardo',
+    aula: [{
+        numero: 1,
+        nome: 'OO'
+      },
+      {
+        numero: 4,
+        nome: 'Javascript'
+      }
+    ]
+  },
+  {
+    nome: 'Nunes',
+    aula: [{
+      numero: 2,
+      nome: 'Banco de Dados I'
+    }]
+  },
+  {
+    nome: 'Pedro (PHP)',
+    aula: [{
+      numero: 3,
+      nome: 'HTML e CSS'
+    }]
+  },
+  {
+    nome: 'Zanatta',
+    aula: [{
+      numero: 5,
+      nome: 'AngularJS'
+    }]
+  }
+];
+
+var aulasEInstrutores = [];
+  
+    for (instrutor of instrutores){
+        for(aulas of instrutor.aula){
+           let adicionar = {numero:aulas.numero,nome:aulas.nome,instrutor:instrutor.nome};
+            aulasEInstrutores.push(adicionar);
+        }
+    }
+    
+    $scope.aulasEInstrutores = aulasEInstrutores;
+    $scope.instrutores = instrutores;
+})
+
