f9297301973dd1ac39613f3ebbc9b09498c995c3
Lucas Damaceno <lucas.damaceno@cwi.com.br>
2017-05-21 18:43:11 -0300

    feat(CRUD):adicionar modificaçao de instrutores e regras adicionais.

--- Modulo5_AngularJS/CRUD/controller.js
+++ Modulo5_AngularJS/CRUD/controller.js
@@ -35,32 +35,52 @@ modulo.controller('controllerCRUD', function($scope,$filter){
         return;
     }
 
+    function getInstrutorByID (ID){
+
+        for(instrutor of instrutores){
+            if(instrutor.id === ID) return instrutor;
+        }
+        return;
+    }
+
 
     $scope.insereAula = function (){
         console.log($scope.aulaAInserir);
         let arrayNomesAulas = [];
         for (var i=0; i < $scope.aulas.length; i++) {
-            arrayNomesAulas +=  $scope.aulas[i].nome;
-            if(arrayNomesAulas.includes($scope.aulaAInserir)) {alert('Aula já cadastrada.'); return;}
+            if($scope.aulas[i].nome.toLowerCase() === $scope.aulaAInserir.toLowerCase()) {alert('Aula já cadastrada.'); return;}
         }
-        let aulaAAdd = {id:ID,nome:$scope.aulaAInserir, estaSendoUtilizada: false};
+        let aulaAAdd = {id:ID,nome:$scope.aulaAInserir, estaSendoUtilizada: $scope.aulaUtilizada || false};
         ID++;
         $scope.aulas.push(aulaAAdd);
     }
     $scope.modificaAula = function(){
+        if($scope.modificarAula.$invalid) {
+            
+            alert('Informações insuficientes.');
+            return;
+            
+        }
         console.log($scope.selecionarAula);
         console.log($scope.novoNome);
+        console.log($scope.modificarAulaUtilizada);
         let arrayNomesAulas = [];
         let indiceAPovoar;
         let povoar = false;
+        let mesmoNome = $scope.selecionarAula.nome.toLowerCase() === $scope.novoNome.toLowerCase();
         for (var i=0; i < $scope.aulas.length; i++) {
-            if($scope.aulas[i].nome.toLowerCase() === $scope.novoNome.toLowerCase()) {alert('Aula já cadastrada.'); return;}
+            if($scope.aulas[i].nome.toLowerCase() === $scope.novoNome.toLowerCase()){ if(mesmoNome === false){alert('Aula já cadastrada.'); return;}}
             if ($scope.aulas[i].id === $scope.selecionarAula.id) {
                 indiceAPovoar = i;
                 povoar = true;
             }
         }
-        if(povoar === true) $scope.aulas[indiceAPovoar].nome = $scope.novoNome;
+        if(povoar === true) {
+            $scope.aulas[indiceAPovoar].nome = $scope.novoNome;
+            $scope.aulas[indiceAPovoar].estaSendoUtilizada = ($scope.modificarAulaUtilizada || false);
+        }
+        
+        
     }
 
     $scope.excluiAula = function(){
@@ -77,10 +97,18 @@ modulo.controller('controllerCRUD', function($scope,$filter){
 
     $scope.insereInstrutor = function(){
         IDInstrutor++;
+        console.log($scope.adicionarInstrutor);
+        if($scope.adicionarInstrutor.$valid === true){
         let aulasAdicionarInstrutor = [];
+        if(typeof $scope.selecionarAulaInstrutor !== 'undefined'){
         for(aula of $scope.selecionarAulaInstrutor){ 
             aulasAdicionarInstrutor.push(aula.id);
         }
+        }
+        
+        
+        
+        
         $scope.fotoInstrutorAInserir = $scope.fotoInstrutorAInserir || 'https://s-media-cache-ak0.pinimg.com/736x/ea/3d/cf/ea3dcff8ed8e8939d98c96b81f747623.jpg';
         let instrutorAdicionar = {id:IDInstrutor,nome:$scope.nomeInstrutorAInserir,sobrenome:$scope.sobrenomeInstrutorAInserir,
                                   email:$scope.emailInstrutorAInserir,idade:$scope.idadeInstrutorAInserir,
@@ -91,21 +119,48 @@ modulo.controller('controllerCRUD', function($scope,$filter){
         {
             $scope.instrutores.push(instrutorAdicionar);
         }
+            return;
+        }
+        alert('Formulário Inválido.');
+    }
+
+    $scope.modificaInstrutor = function(){
+        let aulasAdicionarInstrutor = [];
+        let instrutorPovoar = getInstrutorByID($scope.instrutorAModificar.id);
+        let mesmoNome = $scope.instrutorAModificar.nome ===  instrutorPovoar.nome;
+        let mesmoEmail = $scope.instrutorAModificar.email ===  instrutorPovoar.email;
+        if(typeof ($scope.selecionarAulaATrocar) !== 'undefined')
+        {for(aula of $scope.selecionarAulaATrocar){ 
+            aulasAdicionarInstrutor.push(aula.id);
+        }}
+        $scope.fotoInstrutorTrocar = $scope.fotoInstrutorTrocar|| 'https://s-media-cache-ak0.pinimg.com/736x/ea/3d/cf/ea3dcff8ed8e8939d98c96b81f747623.jpg';
+        let instrutorModificar = {id:$scope.instrutorAModificar.id,nome:$scope.nomeInstrutorTrocar,sobrenome:$scope.sobrenomeInstrutorTrocar,
+                                  email:$scope.emailInstrutorTrocar,idade:$scope.idadeInstrutorTrocar,
+                                  dandoAula:$scope.dandoAulaInstrutorTrocar,urlFoto:$scope.fotoInstrutorTrocar,aula:aulasAdicionarInstrutor}
+        console.log($scope.selecionarAulaATrocar);
+        console.log(instrutorModificar);
+        if((!existeInstrutorComNome($scope.nomeInstrutorTrocar,mesmoNome) || mesmoNome) && (!existeInstrutorComEmail($scope.emailInstrutorTrocar,mesmoEmail) || mesmoEmail))
+        {
+            $scope.instrutores.splice(instrutorModificar.id,1);
+            $scope.instrutores.push(instrutorModificar);
+        }
 
 
     }
 
-    function existeInstrutorComNome(instrutorTestar){
+    function existeInstrutorComNome(instrutorTestar, comparacao){
+        let comparar = comparacao || false;
         for(instrutorTestado of instrutores){
-            if(instrutorTestado.nome === instrutorTestar) { alert('Instrutor com este nome já cadastrado.'); return true;}
+            if(instrutorTestado.nome === instrutorTestar) { if(comparar === false)alert('Instrutor com este nome já cadastrado.'); return true;}
         }
 
         return false;
     }
 
-    function existeInstrutorComEmail(emailTestar){
+    function existeInstrutorComEmail(emailTestar,comparacao){
+        let comparar = comparacao || false;
         for(instrutorTestado of instrutores){
-            if(instrutorTestado.email.toLowerCase() === emailTestar.toLowerCase()) { alert('Instrutor com este email já cadastrado.'); return true;}
+            if(instrutorTestado.email.toLowerCase() === emailTestar.toLowerCase()) { if(comparar === false) alert('Instrutor com este email já cadastrado.'); return true;}
         }
 
         return false;
@@ -117,6 +172,19 @@ modulo.controller('controllerCRUD', function($scope,$filter){
                 $scope.instrutores.splice([i],1);
                 return;
             }
+        }
+
+    }
+
+    $scope.povoarMudancas = function(){
+        if(typeof $scope.instrutorAModificar.id !== 'undefined'){
+            let instrutorPovoar = getInstrutorByID($scope.instrutorAModificar.id);
+            $scope.nomeInstrutorTrocar  = instrutorPovoar.nome;
+            $scope.sobrenomeInstrutorTrocar = instrutorPovoar.sobrenome;
+            $scope.idadeInstrutorTrocar = instrutorPovoar.idade;
+            $scope.emailInstrutorTrocar = instrutorPovoar.email;
+            $scope.dandoAulaInstrutorTrocar = instrutorPovoar.dandoAula;
+            $scope.fotoInstrutorTrocar = instrutorPovoar.urlFoto;
         }
 
     }
