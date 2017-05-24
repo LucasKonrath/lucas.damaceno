04e381ee9aa78f376239575ab52c5201ee1d3f92
Lucas Damaceno <lucas.damaceno@cwi.com.br>
2017-05-24 17:35:04 -0300

    feat(Modulo6_.NET_API):adicionar arquivos feitos em aula

---
+++ Modulo6_.NET_API/Exercicio1/aula1/demo1/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace demo1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var qtdElementos = 0;
+            List<int> entradas = new List<int>();
+           
+            while (true)
+            {
+                
+                Console.WriteLine("Digite um valor");
+                var entrada = Console.ReadLine();
+                if (entrada == "exit") {
+                    int numEntrada = 0;
+                    foreach(var entradaIndividual in entradas)
+                    {
+                        numEntrada++;
+                        Console.WriteLine($"Entrada Número {numEntrada} = {entradaIndividual}");
+                        
+                    }
+
+                    Console.WriteLine("########################FINO");
+                    Console.ReadKey();
+                    return;
+
+                }
+                else
+                {
+                    var numeroEntrada = 0;
+                    bool result = Int32.TryParse(entrada, out numeroEntrada);
+                    if (result)
+                    {
+
+                        entradas.Add(int.Parse(entrada));
+
+                    }
+                    else
+                    {
+                        Console.WriteLine("Conversão falhou.");
+                    }
+                    
+                }
+            }
+
+        }
+    }
+}
---
+++ Modulo6_.NET_API/teste.cs
@@ -0,0 +1,18 @@
+using System;
+
+class Program{
+    
+    
+   static void Main(string[] args)
+   {
+        
+        Console.WriteLine("Hello World");
+        
+        
+    }
+    
+    
+    
+    
+    
+}
