07237f39c5e83dcb71ee89b09e926fc4cf8272fd
Lucas Damaceno <lucas.damaceno@cwi.com.br>
2017-05-29 17:38:08 -0300

    feat(.NET, Aula29/05): adicionar arquivos feitos em aula.

---
+++ Modulo6_.NET_API/Aula2905/Demo1/Demo1.Infraestrutura/Repositorios/PedidoRepositorio.cs
@@ -0,0 +1,22 @@
+﻿using Demo1.Dominio.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo1.Infraestrutura.Repositorios
+{
+    class PedidoRepositorio : IPedidoRepositorio
+    {
+
+        public void Alterar(Pedido pedido)
+        {
+
+            throw new NotImplementedException();
+
+        }
+
+
+    }
+}
