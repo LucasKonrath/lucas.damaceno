d7ff7649d796cfbc7fbbc4d1b2e343c5ae355cb7
Lucas Damaceno <lucas.damaceno@cwi.com.br>
2017-05-31 00:30:10 -0300

    feat(EditoraCrescer):implementar operações com Autores;

---
+++ Modulo6_.NET_API/Aula3005/EditoraCrescer.Infraestrutura/EditoraCrescer.api/Controllers/AutoresController.cs
@@ -0,0 +1,39 @@
+﻿using EditoraCrescer.Infraestrutura;
+using EditoraCrescer.Infraestrutura.Entidades;
+using EditoraCrescer.Infraestrutura.Repositorios;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace EditoraCrescer.api.Controllers
+{
+    public class AutoresController : ApiController
+    {
+        private Contexto contexto = new Contexto();
+
+        private AutorRepositorio repositorio = new AutorRepositorio();
+        public IHttpActionResult Get()  
+        {
+            var autores = repositorio.Obter();
+
+            return Ok(autores);
+        }
+
+        public IHttpActionResult Post(Autor autor)
+        {
+
+            repositorio.Criar(autor);
+            return Ok(autor);
+        }
+
+        public IHttpActionResult Delete(int id)
+        {
+            repositorio.Deletar(id);
+            return Ok();
+        }
+
+    }
+}
---
+++ Modulo6_.NET_API/Aula3005/EditoraCrescer.Infraestrutura/Repositorios/AutorRepositorio.cs
@@ -0,0 +1,43 @@
+﻿using EditoraCrescer.Infraestrutura.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EditoraCrescer.Infraestrutura.Repositorios
+{
+    public class AutorRepositorio
+    {
+
+        private Contexto contexto = new Contexto();
+
+        public AutorRepositorio()
+        {
+
+
+
+        }
+
+        public List<Autor> Obter()
+        {
+            return contexto.Autores.ToList();
+        }
+
+        public void Criar(Autor autor)
+        {
+
+            contexto.Autores.Add(autor);
+            contexto.SaveChanges();
+        }
+
+        public void Deletar(int id)
+        {
+            Autor autorRemover = contexto.Autores.FirstOrDefault(x => x.Id == id);
+            contexto.Autores.Remove(autorRemover);
+            contexto.SaveChanges();
+
+        }
+
+    }
+}
