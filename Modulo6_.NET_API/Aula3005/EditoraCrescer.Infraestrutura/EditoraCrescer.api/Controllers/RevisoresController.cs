6f15f33542be8e830f668fcafc86ca9d6977af7c
Lucas Damaceno <lucas.damaceno@cwi.com.br>
2017-05-31 00:38:08 -0300

    feat(EditoraCrescer):implementar operações envolvendo revisores.

---
+++ Modulo6_.NET_API/Aula3005/EditoraCrescer.Infraestrutura/EditoraCrescer.api/Controllers/RevisoresController.cs
@@ -0,0 +1,39 @@
+﻿using EditoraCrescer.Infraestrutura;
+using EditoraCrescer.Infraestrutura.Entidades;
+using EditoraCrescer.Infraestrutura.Repositorios;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace EditoraCrescer.api.Controllers
+{
+    public class RevisoresController : ApiController
+    {
+        private Contexto contexto = new Contexto();
+
+        private RevisorRepositorio repositorio = new RevisorRepositorio();
+        public IHttpActionResult Get()
+        {
+            var revisores = repositorio.Obter();
+
+            return Ok(revisores);
+        }
+
+        public IHttpActionResult Post(Revisor revisor)
+        {   
+
+            repositorio.Criar(revisor);
+            return Ok(revisor);
+        }
+
+        public IHttpActionResult Delete(int id)
+        {
+            repositorio.Deletar(id);
+            return Ok();
+        }
+
+    }
+}
---
+++ Modulo6_.NET_API/Aula3005/EditoraCrescer.Infraestrutura/Repositorios/RevisorRepositorio.cs
@@ -0,0 +1,43 @@
+﻿using EditoraCrescer.Infraestrutura.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EditoraCrescer.Infraestrutura.Repositorios
+{
+    public class RevisorRepositorio
+    {
+
+        private Contexto contexto = new Contexto();
+
+        public RevisorRepositorio()
+        {
+
+
+
+        }
+
+        public List<Revisor> Obter()
+        {
+            return contexto.Revisores.ToList();
+        }
+
+        public void Criar(Revisor revisor)
+        {
+
+            contexto.Revisores.Add(revisor);
+            contexto.SaveChanges();
+        }
+
+        public void Deletar(int id)
+        {
+            Revisor revisorRemover = contexto.Revisores.FirstOrDefault(x => x.Id == id);
+            contexto.Revisores.Remove(revisorRemover);
+            contexto.SaveChanges();
+
+        }
+
+    }
+}
