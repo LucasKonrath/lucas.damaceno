e0ab902eb299715d5f00178d24ca7592995e9127
Lucas Damaceno <lucas.damaceno@cwi.com.br>
2017-06-05 06:04:08 -0300

    feat(EditoraCrescer):adicionar função excluir livro.

--- Modulo6_.NET_API/EditoraCrescer/EditoraCrescer.Infraestrutura/EditoraCrescer.api/AngularJS/administrativo.controller.js
+++ Modulo6_.NET_API/EditoraCrescer/EditoraCrescer.Infraestrutura/EditoraCrescer.api/AngularJS/administrativo.controller.js
@@ -2,30 +2,42 @@
 modulo.controller('AdministrativoController', function ($scope, editoraService,authService, $routeParams, $location) {
     $scope.obterLivros = obterLivros;
     $scope.insereLivro = insereLivro;
-     $scope.auth = authService;
-
+    $scope.auth = authService;
+    $scope.remover = remover;
+    $scope.revisar = revisar;
     function insereLivro(livro){
         $scope.livro.IDRevisor=21;
         console.log($scope.livro);
         editoraService.criarLivro($scope.livro).then(response =>{
-
-            console.log(response)
+            obterLivros();
+            console.log(response);
         });
 
     }
-    
+
+    function remover(isbn){
+
+        editoraService.removerLivro(isbn).then(
+            response => {
+                obterLivros(); 
+                console.log(response);
+
+            });
+
+    }
+
     $scope.mensagem = {
-    colaborador: 'Mensagem incrível para o usuário AUTENTICADO',
-    editor: 'Mensagem incrível para o usuário EDITOR',
-  };
-    
-    
-    
-     $scope.logout = function (usuario) {
+        colaborador: 'Mensagem incrível para o usuário AUTENTICADO',
+        editor: 'Mensagem incrível para o usuário EDITOR',
+    };
 
-    authService.logout(usuario);
 
-  };
+
+    $scope.logout = function (usuario) {
+
+        authService.logout(usuario);
+
+    };
 
 
     function obterLivros() {
