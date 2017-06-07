8be9e557047a73608d69622a0d8886876f423b6a
Lucas Damaceno <lucas.damaceno@cwi.com.br>
2017-06-07 17:44:33 -0300

    feat(ImobiliariaTriVaga_Modulo6):adicionar arquivos feitos em aula

---
+++ Modulo6_.NET_API/ImobiliariaTriVaga/ImobiliariaTriVaga/Controllers/PedidosController.cs
@@ -0,0 +1,52 @@
+﻿using ImobiliariaTriVaga.Infraestrutura;
+using ImobiliariaTriVaga.Infraestrutura.Entidades;
+using ImobiliariaTriVaga.Infraestrutura.Repositorios;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace ImobiliariaTriVaga.Controllers
+{
+
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
+    [RoutePrefix("api/Pedidos")]
+
+    public class PedidosController : ApiController
+    {
+
+        private Contexto contexto = new Contexto();
+
+        private PedidoRepositorio repositorio = new PedidoRepositorio();
+
+        [Route("obter/{id:int}")]
+        [HttpGet]
+        public HttpResponseMessage ObterPedido(int id)
+        {
+            var pedido = repositorio.Obter(id);
+            if (pedido == null) return Request.CreateResponse(HttpStatusCode.NotFound, new { mensagens = new string[] { "Erro no cadastro." } });
+            return Request.CreateResponse(HttpStatusCode.OK, new { data = pedido });
+        }
+
+        [Route("criar")]
+        [HttpPost]
+        public HttpResponseMessage CriarPedido(Pedido pedido)
+        {
+            var pedidoCriado = repositorio.Criar(pedido);
+            List<int> adicionaisDoPedido = new List<int>();
+            List<int> quantidadeDoAdicional = new List<int>();
+            foreach( var adicional in pedidoCriado.Adicionais)
+            {
+                adicionaisDoPedido.Add(adicional.IdAdicional);
+                quantidadeDoAdicional.Add(adicional.Quantidade);
+            }
+            if (pedidoCriado == null) return Request.CreateResponse(HttpStatusCode.NotFound, new { mensagens = new string[] { "Erro no cadastro." } });
+            return Request.CreateResponse(HttpStatusCode.OK, new { data = new { id = pedidoCriado.Id, IdCliente = pedidoCriado.IdCliente, IdTipoImovel = pedidoCriado.IdTipoImovel,
+                DataPrevista = pedidoCriado.DataEntregaPrevista, adicionais = new { adicionais = adicionaisDoPedido, quantidadeDoAdicional = quantidadeDoAdicional }  } });
+        }
+
+    }
+}
