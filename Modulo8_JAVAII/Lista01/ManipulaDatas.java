15e4d1be106845b4dcd9d9d312a954ee905eb8d2
Lucas Damaceno <lucas.damaceno@cwi.com.br>
2017-06-21 08:50:56 -0300

    feat(Lista01_Java2):adicionar DateUtils e metodo dia_semana.

---
+++ Modulo8_JAVAII/Lista01/ManipulaDatas.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.crescer.lista01;
+import java.util.*;
+/**
+ *
+ * @author lucas
+ */
+public class ManipulaDatas implements CalendarUtils{
+    
+    @Override
+    public DiaSemana diaSemana(Date dataRecebida){
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(dataRecebida);
+        int diaDaSemana = calendar.get(Calendar.DAY_OF_WEEK);
+        DiaSemana diaARetornar = DiaSemana.DOMINGO;
+        switch(diaDaSemana){
+            case 1:
+                diaARetornar = DiaSemana.DOMINGO;
+                break;
+            case 2:
+                diaARetornar = DiaSemana.SEGUNDA_FEIRA;
+                break;
+            case 3:
+                diaARetornar = DiaSemana.TERCA_FEIRA;
+                break;
+            case 4:
+                diaARetornar = DiaSemana.QUARTA_FEIRA;
+                break;
+            case 5:
+                diaARetornar = DiaSemana.QUINTA_FEIRA;
+                break;
+            case 6:
+                diaARetornar = DiaSemana.SEXTA_FEIRA;
+                break;
+            case 7:
+                diaARetornar = DiaSemana.SABADO;
+                break;
+            default:
+                System.out.println("Ops. Erro");
+                break;
+        }
+        return diaARetornar;
+    }
+    
+    @Override
+    public String tempoDecorrido (Date dataRecebida){
+    
+        return ("Provisorio");
+    }
+}
