090b5a04bf503886d680b902fe9b9fea579077b4
Lucas Damaceno <lucas.damaceno@cwi.com.br>
2017-06-22 16:09:46 -0300

    refactor(AULA03_JAVA2):adicionar classes de JDBC refatoradas.

--- Modulo8_JAVAII/lista02/src/main/java/br/com/crescer/lista02/ClassePrincipal.java
+++ Modulo8_JAVAII/lista02/src/main/java/br/com/crescer/lista02/ClassePrincipal.java
@@ -23,12 +23,10 @@ public class ClassePrincipal {
             System.out.println(repo.ls("TestFolder"));
             repo.rm("TestFolder\\Testando2.txt");
             repo.mv("TestFolder\\Testando.txt", "C:\\Users\\lucas.damaceno\\Documents\\lucas.damaceno\\lucas.damaceno\\Modulo8_JAVAII\\lista02\\TestMoveFile");
-            
             System.out.println(reader.read("BlackParade.txt"));
             System.out.println(reader.read("MusicaMasTristeDoLinkPark.xml"));
             System.out.println(reader.read("pom.xml"));
-            writer.write("CincoDeNovembro.txt", "Remember, remember.\n The Fifth of November.\n  The Gunpowder, treason and plot. \n I know of no reason, the Gunpowder treason \n SHALL EVER BE FORGOT.");
-            
+            writer.write("CincoDeNovembro.txt", "Remember, remember.\n The Fifth of November.\n  The Gunpowder, treason and plot. \n I know of no reason, the Gunpowder treason \n SHALL EVER BE FORGOT.");  
             writer.write("ArquivoQueNaoExiste.txt","deve falhar");
             writer.write("ArquivoNaoTxt.xml","falha de novo");
             
