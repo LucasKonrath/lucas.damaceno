d94afa404222a08accbf4956e25cf6bbcaf3108d
Lucas Damaceno <lucas.damaceno@cwi.com.br>
2017-07-05 10:12:37 -0300

    feat(TCCeller):adicionar envio de mensagem no Slack pelo C#.

---
+++ TCCeller_test/testeEmail/TesteEnvioSlack/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TesteEnvioSlack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            WebRequest request = WebRequest.Create("https://slack.com/api/chat.postMessage?token=xoxp-166684486960-167730704512-207888517604-333d604d7b051b2c7138d140e39ea825&channel=G634US12L&text=tem%20que%20ser%20bem%20duro&pretty=1");
+            WebResponse response = request.GetResponse();
+            Console.WriteLine(response);
+            response.Close();
+        }
+    }
+}
