1e7c41dafa9866b0f68f728e1df8d1a246f16899
Lucas Damaceno <lucas.damaceno@cwi.com.br>
2017-07-05 09:20:08 -0300

    feat(TCCeller):adicionar exemplo funcional de código de envio de email.

---
+++ TCCeller_test/testeEmail/testeEmail/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testeEmail
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            try
+            {
+                MailMessage mail = new MailMessage();
+                SmtpClient SmtpServer = new SmtpClient("smtp.gmail.com");
+
+                mail.From = new MailAddress("cellercwi@gmail.com");
+                mail.To.Add("lucaskdamaceno@gmail.com");
+                mail.Subject = "Test Mail";
+                mail.Body = "This is for testing SMTP mail from GMAIL";
+
+                SmtpServer.Port = 587;
+                SmtpServer.Credentials = new System.Net.NetworkCredential("cellercwi@gmail.com", "cellercwi.10");
+                SmtpServer.EnableSsl = true;
+
+                SmtpServer.Send(mail);
+                Console.WriteLine("mail Send");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+
+        }
+    }
+}
